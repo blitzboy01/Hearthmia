725e38d6dddb6e2f480f93d64f158c574fae57f3
blitzboy01 <francoisbelmi@gmail.com>
2020-01-18 18:09:12 +0800

    Added History, Help and modified Home Fragment

--- app/src/main/java/com/soumio/inceptiontutorial/Home/HomeFragment.kt
+++ app/src/main/java/com/soumio/inceptiontutorial/Home/HomeFragment.kt
@@ -10,6 +10,8 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.findNavController
 import com.soumio.inceptiontutorial.Analyze.ChooseModel
+import com.soumio.inceptiontutorial.Help.HelpFragment
+import com.soumio.inceptiontutorial.History.HistoryFragment
 import com.soumio.inceptiontutorial.R
 
 class HomeFragment : Fragment() {
@@ -24,15 +26,27 @@ class HomeFragment : Fragment() {
             it.findNavController().navigate(R.id.action_nav_home_to_nav_ecgbasics)
         }
 
-        val btnAnalyzeButton: ImageButton = root.findViewById(R.id.analyze_button)
-        btnAnalyzeButton.setOnClickListener {
+        val btnAnalyze: ImageButton = root.findViewById(R.id.analyze_button)
+        btnAnalyze.setOnClickListener {
             Intent(context, ChooseModel::class.java).also { intent ->
                 startActivity(intent)
-
             }
 //            it.findNavController().navigate(R.id.action_nav_home_to_nav_analyze)
         }
 
+        val btnHistory: ImageButton = root.findViewById(R.id.history_button)
+        btnHistoryButton.setOnClickListener {
+            Intent(context, HistoryFragment::class.java).also { intent ->
+                startActivity(intent)
+            }
+        }
+
+        val btnHelp: ImageButton = root.findViewById(R.id.help_button)
+        btnHelp.setOnClickListener {
+            Intent(context, HelpFragment::class.java).also { intent ->
+                startActivity(intent)
+            }
+        }
 
         return root
 
